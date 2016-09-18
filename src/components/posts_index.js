7939836481b633700106b918234df90258420efe
kurtispritchard <kpritch11@gmail.com>
2016-09-18 10:26:50 -0500

    Added componentWillMount lifecycle method

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class PostsIndex extends Component {
+    componentWillMount() {
+        console.log('this would be a good time to call an action creator to fetch posts');
+    }
     render() {
         return (
             <div>List of blog posts</div>
