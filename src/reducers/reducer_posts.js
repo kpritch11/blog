99bb2cbaaa9173f7fd46f4ace9b2c4d6decf7367
kurtispritchard <kpritch11@gmail.com>
2016-09-18 09:47:28 -0500

    Updated posts reducer comment

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = { all: [], post: null };
 export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
     case FETCH_POSTS:
-        return { ...state, all: action.payload.data } // create new object, take current state, add on all: action.payload.data
+        return { ...state, all: action.payload.data } // create new object, take current state, assign all: action.payload.data, return new state
     default:
         return state;
     }
