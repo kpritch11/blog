ee159179f0633098fd981f4936791cf5b47866e7
kurtispritchard <kpritch11@gmail.com>
2016-10-02 10:36:25 -0500

    Added form validation to turn inputs red. Added cancel button to navigate back to list of blog posts

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
     render() {
@@ -13,28 +14,29 @@ class PostsNew extends Component {
         return (
             <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create A New Post</h3>
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} /> {/* {...title} breaks down title and passes in all properites (onChange, onFocus, active, etc) */}
-                        <div className="text-help">
-                            {title.touched ? title.error : ''}
-                        </div>
+                    <div className="form-control-label">
+                        {title.touched ? title.error : ''}
+                    </div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories} />
-                        <div className="text-help">
-                            {categories.touched ? categories.error : ''}
-                        </div>
+                    <div className="form-control-label">
+                        {categories.touched ? categories.error : ''}
+                    </div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea className="form-control" {...content} />
-                        <div className="text-help">
-                            {content.touched ? content.error : ''}
-                        </div>
+                    <div className="form-control-label">
+                        {content.touched ? content.error : ''}
+                    </div>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -50,7 +52,7 @@ function validate(values) {
         errors.categories = 'Enter a category';
     }
     if (!values.content) {
-        errors.content = 'Enter content';
+        errors.content = 'Enter some content';
     }
 
     return errors;
